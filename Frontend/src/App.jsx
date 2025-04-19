80274edf64a7d6c130ff5742f2ffe69ba7c998e0
ManmohanSinghRaghav <manmohan0singh0@gmail.com>
2025-04-19 13:23:04 +0530

    Update title and modify API script paths; remove unused SVG file

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 
 function App() {
   return (
-    <p className='text-4xl bg-red-500 text-center p-2'>G Viz</p>
+    <p className='text-4xl border-2 border-orange-500 bg-red-500 text-center p-2'>Hello</p>
   )
 }
 
